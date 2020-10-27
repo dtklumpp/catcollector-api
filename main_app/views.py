99f08866e5fa0efcf29410995f46096fba1e046c
Dalton <daltonvaren@gmail.com>
2020-10-27 12:08:21 -0700

    updates

--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 # --- Base Views ---
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -22,6 +24,8 @@ def api(request):
 # --- Cat Views ---
 
 # index and create
+
+
 def cats_index(request):
     if request.method == 'POST':
         cat_form = Cat_Form(request.POST)
@@ -38,52 +42,63 @@ def cats_index(request):
     return render(request, 'cats/index.html', context)
 
 # show
+
+
 def cats_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     # [3,1] is what is returned cat.toys.all().values_list('id')
-    toys_cat_doesnt_have = Toy.objects.exclude(id__in=cat.toys.all().values_list('id')) # get all toys where the cat id is not associated in the jion table
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=cat.toys.all().values_list(
+        'id'))  # get all toys where the cat id is not associated in the jion table
     feeding_form = Feeding_Form()
-    context = {'cat': cat, 'feeding_form': feeding_form, 'available_toys': toys_cat_doesnt_have}
+    context = {'cat': cat, 'feeding_form': feeding_form,
+               'available_toys': toys_cat_doesnt_have}
     return render(request, 'cats/detail.html', context)
 
 # edit && update
+
+
 def cats_edit(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  if request.method == 'POST':
-    cat_form = Cat_Form(request.POST, instance=cat)
-    if cat_form.is_valid():
-      cat_form.save()
-      return redirect('detail', cat_id=cat_id)
-  else:  
-    # in form(instance=The object that we pull back from db)
-    cat_form = Cat_Form(instance=cat)
-  context = {'cat': cat, 'cat_form': cat_form}
-  return render(request, 'cats/edit.html', context)
+    cat = Cat.objects.get(id=cat_id)
+    if request.method == 'POST':
+        cat_form = Cat_Form(request.POST, instance=cat)
+        if cat_form.is_valid():
+            cat_form.save()
+            return redirect('detail', cat_id=cat_id)
+    else:
+        # in form(instance=The object that we pull back from db)
+        cat_form = Cat_Form(instance=cat)
+    context = {'cat': cat, 'cat_form': cat_form}
+    return render(request, 'cats/edit.html', context)
 
 # delete
+
+
 def cats_delete(request, cat_id):
     Cat.objects.get(id=cat_id).delete()
     return redirect("cats_index")
 
 # --- Feeding Views ---
 
+
 def add_feeding(request, cat_id):
-  feeding_form = Feeding_Form(request.POST)
-  if feeding_form.is_valid():
-    new_feeding = feeding_form.save(commit=False)
-    new_feeding.cat_id = cat_id
-    new_feeding.save()
-  return redirect('detail', cat_id=cat_id)
+    feeding_form = Feeding_Form(request.POST)
+    if feeding_form.is_valid():
+        new_feeding = feeding_form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
 
 # --- Toy Views ---
+
 
 def assoc_toy(request, cat_id, toy_id):
     Cat.objects.get(id=cat_id).toys.add(toy_id)
     # Cat.objects.get(id=cat_id) return Cat(1) Eldritch
-    # Cat.toys returns the join table 
+    # Cat.toys returns the join table
     # table.add -> creates an row in the join table the id provided
-    # cat 1 | toy 3 
+    # cat 1 | toy 3
     return redirect('detail', cat_id=cat_id)
+
 
 def deassoc_toy(request, cat_id, toy_id):
     Cat.objects.get(id=cat_id).toys.remove(toy_id)
@@ -91,16 +106,18 @@ def deassoc_toy(request, cat_id, toy_id):
 
 # --- Signup View ---
 
+
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('cats_index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('cats_index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
